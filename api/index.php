3d43b140bca4ef0371b2b8ec3ba36723fd563dc7
Biltu Das <billionto@gmail.com>
2023-05-06 23:54:08 +0530

    updated php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,11 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    if(strtolower($_GET['format']) == 'text'){
-        $text = shell_exec("python ./python/textparse.py " . strtolower($_GET['email']));
+if(isset($_REQUEST['format']) && !empty($_REQUEST['format'])){
+    if(strtolower($_REQUEST['format']) == 'text'){
+        $text = shell_exec("python ./python/textparse.py " . strtolower($_REQUEST['email']));
         echo $text;
-    } else {
-        $json = shell_exec("python ./python/jsonparse.py " . strtolower($_GET['email']));
-        echo $json;
     }
-}
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    if(strtolower($_POST['format']) == 'text'){
-        $text = shell_exec("python ./python/textparse.py " . strtolower($_POST['email']));
-        echo $text;
-    } else {
-        $json = shell_exec("python ./python/jsonparse.py " . strtolower($_POST['email']));
-        echo $json;
-    }
+} else {
+    $json = shell_exec("python ./python/jsonparse.py " . strtolower($_REQUEST['email']));
+    echo $json;
 }
 ?>
